02cea275ddfc0835f3372867fd7be60bbcbdd5a2
Allison Schulman <68669789+sneakin-allie@users.noreply.github.com>
2020-12-20 15:35:11 -0500

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Card.css';
+
+function Card(props) {
+    return (
+        <div className="Card">
+            <button
+                type="button"
+                onClick={() => props.onClickDelete(props.id)}
+            >
+                delete
+            </button>
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    )
+}
+
+Card.propTypes = {
+    onClickDelete: () => {}
+}
+
+export default Card; 
---
+++ src/List.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './List.css';
+import Card from './Card';
+
+function List(props) {
+    return (
+        <section className="List">
+            <header className="List-header">
+                <h2>Fourth list</h2>
+            </header>
+            <div className="List-cards">
+                {props.cards.map(card =>
+                    <Card 
+                        key={card.id}
+                        id={card.id}
+                        title={card.title}
+                        content={card.content}
+                        onClickDelete={props.onClickDelete}
+                    />
+                )}
+                <button
+                    type="button"
+                    className="List-add-button"
+                    onClick={() => props.onClickAdd(props.id)}
+                >
+                    + Add Random Card
+                </button>
+            </div>
+        </section>
+    )
+}
+
+List.defaultProps = {
+    onClickAdd: () => {}
+}
+
+export default List;
